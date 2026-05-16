afdbc03d65d3e3b7a65d5384e956b9d2fbfa99c6
shivans-16 <shivans0018@gmail.com>
2026-05-17 00:37:28 +0530

    Fix: Try port 465 for Brevo SMTP

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,9 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
+    
     host: 'smtp-relay.brevo.com',
-    port: 587,
-    secure: false,
+    port: 465,        
+    secure: true,     
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
